aa21213849bab7b45bda48c286904cfd9be0a3d7
Clint171 <clintsimiyu004@gmail.com>
2023-12-23 14:37:01 +0300

    fix: stringified credentials

--- js/google.js
+++ js/google.js
@@ -4,7 +4,7 @@ function onSignUp(credentials) {
     if (credentials === null) {
         return;
     }
-    alert(credentials);
+    alert(JSON.stringify(credentials));
     var profile = credentials.payload
     form.firstName.value = profile.given_name;
     form.lastName.value = profile.family_name;
