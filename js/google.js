04fda796067ccc186ec300cf1a7727d61909bc7b
Clint171 <clintsimiyu004@gmail.com>
2023-12-23 14:55:02 +0300

    chore: refactor

--- js/google.js
+++ js/google.js
@@ -4,7 +4,7 @@ function onSignUp(credentials) {
     if (credentials === null) {
         return;
     }
-    alert(JSON.stringify(credentials));
+    alert(credentials.credential);
     var profile = credentials.payload
     form.firstName.value = profile.given_name;
     form.lastName.value = profile.family_name;
