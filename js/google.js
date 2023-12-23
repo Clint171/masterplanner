4eb122471a638b4b4e6ee1bcff99acc4b2b2cd31
Clint171 <clintsimiyu004@gmail.com>
2023-12-23 13:55:24 +0300

    fix: autofill form on google signin

--- js/google.js
+++ js/google.js
@@ -1,24 +1,14 @@
+let form = document.querySelector('form');
+
 // google sign up
 function onSignUp(credentials) {
     console.log(credentials);
     var profile = credentials.payload
-    let fetchOptions = {
-        headers : {
-            'Content-Type' : "application/json"
-        },
-        method : 'POST',
-        body : JSON.stringify({
-            "firstName" : profile.given_name,
-            "lastName" : profile.family_name,
-            "defaultEmail" : profile.email,
-            "password"  : profile.sub
-        })
-    }
-    alert(fetchOptions.body);
-    fetch("https://masterplanner.onrender.com/signup" , fetchOptions).then(()=>{
-        alert("Account created successfully");
-        window.location.href = "/verify-email.html"
-    });
+    form.firstName.value = profile.given_name;
+    form.lastName.value = profile.family_name;
+    form.defaultEmail.value = profile.email;
+    form.password.value = profile.sub;
+    form.confirm.value = profile.sub;
 }
 
 // google log in
