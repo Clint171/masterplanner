4572bd6319fe26b39b00dbe68cd34582c7077345
Clint171 <clintsimiyu004@gmail.com>
2023-12-23 12:07:50 +0300

    fix: fixed google login

--- js/signup.js
+++ js/signup.js
@@ -26,3 +26,25 @@ form.addEventListener('submit' , (event)=>{
         alert("Password and confirmation do not match!");
     }
 });
+
+// google sign in
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    let fetchOptions = {
+        headers : {
+            'Content-Type' : "application/json"
+        },
+        method : 'POST',
+        body : JSON.stringify({
+            "firstName" : profile.getGivenName(),
+            "lastName" : profile.getFamilyName(),
+            "defaultEmail" : profile.getEmail(),
+            "password"  : "google"
+        })
+    }
+    alert(fetchOptions.body);
+    fetch("https://masterplanner.onrender.com/signup" , fetchOptions).then(()=>{
+        alert("Account created successfully");
+        window.location.href = "/verify-email.html"
+    });
+}
